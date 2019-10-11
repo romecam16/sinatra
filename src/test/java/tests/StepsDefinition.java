88c348474f30af6226c5fbc88db07d0cd40e718d
Camilo Ernesto Romero López <romecam16@gmail.com>
2019-10-10 20:55:39 -0500

    It was addded cucumber test for sinatra project

--- src/test/java/tests/StepsDefinition.java
+++ src/test/java/tests/StepsDefinition.java
@@ -1,34 +1,71 @@
 package tests;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class StepsDefinition {
 
+    public WebDriver driver;
+
     @Given("usuario abre la aplicacion")
     public void usuario_abre_la_aplicacion() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://songs-by-sinatra.herokuapp.com");
+
     }
 
     @When("usuario diligencia las credenciales")
     public void usuario_diligencia_las_credenciales() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        WebElement loginLink = driver.findElement(By.xpath("//a[@href='/login']"));
+        loginLink.click();
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value = 'Log In']"));
+
+        usernameField.sendKeys("frank");
+        passwordField.sendKeys("sinatra");
+        loginButton.click();
+
     }
 
     @Then("usuario debe estar en el home page")
-    public void usuario_debe_estar_en_el_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void usuario_debe_estar_en_el_home_page() throws Exception {
+
+        String url = driver.getCurrentUrl();
+        if (url.equals("https://songs-by-sinatra.herokuapp.com/")) {
+            System.out.println("Test passed");
+        } else {
+            throw new Exception("URL no es homepage" + url);
+        }
     }
 
     @And("usuario visualiza el mensaje de bienvenida")
-    public void usuario_visualiza_el_mensaje_de_bienvenida() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void usuario_visualiza_el_mensaje_de_bienvenida() throws Exception {
+
+        WebElement loggedInMessage = driver.findElement(By.id("flash"));
+        if (loggedInMessage.getText().equals("You are now logged in as frank"))
+            System.out.println("Test Passed");
+        else {
+            throw new Exception("Mensaje de bienvenida no està presente");
+        }
+
+
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
     }
 
 }
